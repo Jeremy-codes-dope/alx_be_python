54d0e8733d45332c13c95b93c08988ea0e9bc1ad
Jeremy-codes <ermiasmehreteab026@gmail.com>
2025-07-19 00:30:12 +0300

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,18 +1,15 @@
 task = input("Enter your task: ")
-priority_level = input("Priority (high/medium/low): ")
+priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
-match (priority_level.lower(), time_bound.lower()):
-    case ("high", "yes"):
-        print("This task is urgent and should be done immediately.")
-    case ("high", "no"):
-        print("This task is important but can be scheduled later.")
+match priority :
+    case ("high"):
+        if time_bound == "yes":
+            print("This task is urgent and should be done immediately.")
+        elif time_bound == "no":
+            print("This matters, do it as soon as you get time.")
     case ("medium", "yes"):
         print("This task should be done soon, but it's not urgent.")
-    case ("medium", "no"):
-        print("This task can be planned for a later time.")
     case ("low", "yes"):
         print("This task is not urgent and can be done at your convenience.")
-    case ("low", "no"):
-        print("This task can be postponed indefinitely.")
     case _:
         print("Invalid input. Please check the priority level and time-bound status.")
